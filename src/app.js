045cdaf1cc7a5241e319d4b81df4ed1e9a6b41f4
jiang1991 <jiang12196@126.com>
2018-01-28 11:30:27 +0800

    add css-loader and Component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import FirstComponent from './container/FirstComponent';
+import s from './app.css';
 
-const App = () => {
-	console.log('jiang');
-	return (<div>hello world</div>);
-};
+console.log(s)
+
+class App extends Component {
+	render() {
+		return (
+			<div className={'app'}>
+				<h2>welcome to my Website</h2>
+				<FirstComponent />
+			</div>
+		);
+	}
+}
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
